b04fabe724ff027db2648f08dfeb3de26dcf2e48
Carlo Yannuzzi <carlogyannuzzi@gmail.com>
2024-02-27 18:57:56 -0500

    added num regular expression contraint, to match agains integers(0-9).

--- main.py
+++ main.py
@@ -18,9 +18,15 @@ def generate_password(length,nums, special_chars, uppercase, lowercase):
         # Generate password
         for _ in range(length):
             password += secrets.choice(all_characters)
-        constraints = [(nums, "")]
+        constraints = [
+            (nums, '[0123456789]')
+        ]
 
     return password
 
 # new_password = generate_password(8)
 # print(new_password)
+
+pattern = 'w[ha]'
+quote = 'Not all those who wander are lost.'
+print(re.search(pattern,quote))
